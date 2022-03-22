53f8853d75a4960ed401fb8c9cabcebbd11295d8
Paulo Perez <pauloperez@bittronic.net>
2022-03-22 14:37:59 -0500

    insert map in contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -19,7 +19,9 @@
 
     <section id="map-section">
         <div class="container">
-            <div id="gmap"></div>
+            <div id="gmap">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3924.1200695927364!2d-75.51553833516526!3d10.412037468660728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef6259afece5ee7%3A0xcd4520aa38b81aae!2sMar%C3%ADa%20Auxiliadora%2C%20Cartagena%20de%20Indias%2C%20Provincia%20de%20Cartagena%2C%20Bol%C3%ADvar!5e0!3m2!1ses!2sco!4v1647977815619!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
             <div class="contact-info">
                 <h2>Contacts</h2>
                 <address>
